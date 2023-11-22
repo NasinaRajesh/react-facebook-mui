40d4ff6bca34d44b54ddc726181b834ff146c647
nasina <rajesh123@gmail.com>
2023-11-22 10:51:19 +0530

    Seperate the user routes and controllers

--- react-facebook-backend/routes/users.js
+++ react-facebook-backend/routes/users.js
@@ -56,168 +56,21 @@ router.delete('/delete-post/', FacebookController.deletePost);
 
 
 // Define the route for getting all user details
-router.get('/all-users', async (req, res) => {
-  try {
-    const users = await FacebookModel.find({}, 'firstName lastName email profilePicture addFriend friendRequests');
-    // Send the retrieved user data as a response
-    res.status(200).json(users); 
-    console.log(users.addFriend)
-  } catch (error) {
-    console.error(error);
-    // Handle the error
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+router.get('/all-users', FacebookController.getAllUsers);
 
 // Define the route for deleting a user account by ID
-router.delete('/delete-user/:userId', async (req, res) => {
-  try {
-    const userId = req.params.userId;
+router.delete('/delete-user/:userId',FacebookController.deleteAccount);
 
-    // Find the user by their ID and delete it
-    const deletedUser = await FacebookModel.findByIdAndDelete(userId);
+router.post('/add-friend', FacebookController.addFriend);
 
-    if (!deletedUser) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
-    // Optionally, you can perform additional cleanup tasks here
-    // (e.g., delete user-related data, revoke tokens, etc.)
-
-    res.status(200).json({ message: 'User deleted successfully' });
-  } catch (error) {
-    console.error(error);
-    // Handle the error
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-router.post('/add-friend', async (req, res) => {
-  try {
-    const { userId, friendId } = req.query; // Assuming you send the user and friend IDs in the request body
-    const {username, profilePicture} = req.body ;
-    const user = await FacebookModel.findById(userId); // Find the user who wants to add a friend
-    const friend = await FacebookModel.findById(friendId); // Find the friend
-    const email = user.email ;
-    if (!user || !friend) {
-      return res.status(404).json({ message: 'User or friend not found' });
-    }
-
-     // Check if the friendRequest is already in the user's friendRequest array
-     const isFriendRequestAlreadyAdded = friend.friendRequests.some((friendDetails) => friendDetails.username.toLowerCase().trim() === username.toLowerCase().trim());
-
-     if (isFriendRequestAlreadyAdded) {
-       return res.status(400).json({ message: 'Friend request already sent' });
-     }
-
-    // Add the friend to the user's `friends` array
-    const friendDetails = {
-      userId,
-      username,
-      profilePicture,
-      email
-    }
-    //console.log(friendDetails)
-    friend.friendRequests.push(friendDetails);
-    await friend.save();
-
-    return res.status(200).json({ message: 'Friend added successfully' });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Error adding friend' });
-  }
-});
-
-router.get('/get-friends/:userId', async (req, res) => {
-  try {
-    const userId = req.params.userId; // Get the user ID from the request parameters
-
-    const user = await FacebookModel.findById(userId); // Populate the 'friends' field
-    // console.log(user)
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    const friendRequests = user.friendRequests; // Extract the list of friends
-    //console.log(friendRequests, "get-friends")
-    return res.status(200).json({ friendRequests });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Error getting friends' });
-  }
-});
+router.get('/get-friends/:userId', FacebookController.friendRequests );
 
 // DELETE route to reject a friend request
-router.delete('/reject-friend-requests', async (req, res) => {
-  //const requestId = parseInt(req.params.id);
-  try{
-  const {userId, requestId} = req.query 
-  //console.log(userId, requestId)
-  const user =  await FacebookModel.findById(userId) ;
-  // Find the index of the request with the provided ID
-  const index = user.friendRequests.findIndex((request) => request.userId == requestId);
-  console.log(index)
-  if (index === -1) {
-    return res.status(404).json({ message: 'Friend request not found' });
-  }
+router.delete('/reject-friend-requests', FacebookController.rejectFriendRequest );
 
-  // Remove the request from the array
-  user.friendRequests.splice(index, 1);
-  await user.save()
-  //console.log(user.friendRequests, "friendRequests array")
-  return res.json({ message: 'Friend request rejected' });
-}catch(error){
-  console.error(error);
-    return res.status(500).json({ message: 'Error Reject friend' });
-}
-});
+router.post('/accept-friend-request', FacebookController.acceptFriendRequest ) ;
 
-router.post('/accept-friend-request', async (req,res)=>{
-  const {userId, requestId} = req.query ;
-  console.log(userId, requestId)
-try{
-  const user = await FacebookModel.findById(userId) ;
-
-  if(!user){
-    return res.status(404).json({message : 'User Not Found'})
-  }
-  user.friends.push(req.body)
-  
-  const index = user.friendRequests.findIndex((request)=> request.userId == requestId) ;
-  user.friendRequests.splice(index,1) ;
-  await user.save()
-  console.log(user.friends, "accept friend")
-  console.log(user.friendRequests, "friend requests")
-  return res.status(200).json({message: 'Friend request accepted'})
-  
-} catch(error){
-  console.log(error) ;
-  return res.status(500).json({message: 'Error while accepting friend request'})
-}
-  
-})
-
-router.patch('/add-friend-button/:userId', async (req,res)=>{
-  const userId = req.params.userId ;
-  console.log(userId)
-  try{
-    const user = await FacebookModel.findById(userId) ; 
-
-    if(!user){
-      return res.status(404).json({message : 'User not found'})
-    } 
-
-    user.addFriend = !user.addFriend ;
-    await user.save() ;
-    console.log(user.addFriend) ;
-    const addFriend = user.addFriend ;
-    return res.status(200).json({addFriend})
-
-  } catch(error) {
-    console.log(error) ;
-    return res.status(500).json({message : 'Error while updating the addFriend button text'})
-  }
-})
+router.patch('/add-friend-button/:userId', FacebookController.addFriendButtonTextChange ) ;
 
 
 
